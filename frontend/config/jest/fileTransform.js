dca8f4631bc7dd3848b61fe052422f67b87e8cee
Ethan <gyi798107@gmail.comm>
2024-12-01 01:32:42 +0630

    Update Jest configuration with proper TypeScript and React support

---
+++ frontend/config/jest/fileTransform.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    process(src, filename) {
+        const assetFilename = JSON.stringify(path.basename(filename));
+        return {
+            code: `module.exports = ${assetFilename};`
+        };
+    },
+    getCacheKey() {
+        return 'fileTransform';
+    },
+}; 
